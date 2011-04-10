88c0637b46547b02a5f57afd845b0f460b009b80
Lissachenko Alexander <lisachenko.it@gmail.com>
2011-04-10 17:29:48 +0400

    Add base exception for aop

---
+++ lib/go/aop/exception/Aspect.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2011, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace go\aop\exception;
+
+/**
+ * Base class for all AOP exceptions
+ *
+ * @package go
+ * @subpackage aop
+ */
+class Aspect extends \RuntimeException
+{
+
+}
