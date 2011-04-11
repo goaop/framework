22c36680823b11fcb2dc4ca7a27981c8c02254b5
Lissachenko Alexander <lisachenko.it@gmail.com>
2011-04-11 21:07:34 +0400

    Add Invocation interface

---
+++ lib/go/aop/Invocation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2011, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace go\aop;
+
+/**
+ * Invocation base interface
+ *
+ * @package go
+ * @subpackage aop
+ */
+interface Invocation extends Joinpoint
+{
+
+}
