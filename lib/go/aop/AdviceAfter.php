ac106d3e614699685c7f4aad271616e42cf2a160
Lissachenko Alexander <lisachenko.it@gmail.com>
2011-04-25 21:24:45 +0400

    Add after, before and around tag interfaces

---
+++ lib/go/aop/AdviceAfter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2011, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace go\aop;
+
+/**
+ * Tag class for all after advices either for field access or method calling
+ * @author Lissachenko Alexander
+ */
+interface AdviceAfter extends \org\aopalliance\aop\Advice
+{
+
+}
