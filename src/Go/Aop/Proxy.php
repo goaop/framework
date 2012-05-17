b1f1a9028609b2a7d8612439fe42933ca77af48e
Lissachenko Alexander <lisachenko.it@gmail.com>
2012-05-17 14:19:13 +0300

    General proxy interface marker

---
+++ src/Go/Aop/Proxy.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2011, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace Go\Aop;
+
+/**
+ * Marker interface implemented by all AOP proxies.
+ *
+ * Used to detect whether or not objects are Go-generated proxies.
+ */
+interface Proxy
+{
+
+}
