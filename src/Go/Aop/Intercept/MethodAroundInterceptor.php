f0d6844df6d5452713e5e0f37133ebae52ffb72d
Lissachenko Alexander <lisachenko.it@gmail.com>
2012-05-22 17:24:01 +0300

    Method around interceptor

---
+++ src/Go/Aop/Intercept/MethodAroundInterceptor.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2011, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace Go\Aop\Framework;
+
+use Exception;
+
+use Go\Aop\AdviceAround;
+use Go\Aop\Intercept\MethodInvocation;
+use Go\Aop\Intercept\MethodInterceptor;
+
+
+/**
+ * @package go
+ */
+class MethodAroundInterceptor extends BaseInterceptor implements MethodInterceptor, AdviceAround
+{
+    /**
+     * Around invoker
+     *
+     * @param $invocation MethodInvocation the method invocation joinpoint
+     * @return mixed the result of the call to {@link Joinpoint::proceed()}
+     * @throws Exception
+     */
+    final public function invoke(MethodInvocation $invocation)
+    {
+        return $this->invokeAdviceForJoinpoint($invocation);
+    }
+}
