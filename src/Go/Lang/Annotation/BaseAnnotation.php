18d7b552bea96f61d621634a1e1bdfd36b0c7d29
Lissachenko Alexander <lisachenko.it@gmail.com>
2012-08-27 09:38:50 +0300

    Change BaseAnnotation for integration with Doctrine annotations library

--- src/Go/Lang/Annotation/BaseAnnotation.php
+++ src/Go/Lang/Annotation/BaseAnnotation.php
@@ -13,14 +13,47 @@ namespace Go\Lang\Annotation;
  */
 abstract class BaseAnnotation
 {
-    public function __construct(array $values)
-    {
-        foreach ($values as $k => $v) {
-            if (!method_exists($this, $name = 'set'.$k)) {
-                throw new \RuntimeException(sprintf('Unknown key "%s" for annotation "@%s".', $k, get_class($this)));
-            }
+    /**
+     * Value property. Common among all derived classes.
+     *
+     * @var string
+     */
+    public $value;
 
-            $this->$name($v);
+    /**
+     * Constructor
+     *
+     * @param array $data Key-value for properties to be defined in this class
+     */
+    public final function __construct(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
         }
+    }
+
+    /**
+     * Error handler for unknown property accessor in Annotation class.
+     *
+     * @param string $name Unknown property name
+     */
+    public function __get($name)
+    {
+        throw new \BadMethodCallException(
+            sprintf("Unknown property '%s' on annotation '%s'.", $name, get_class($this))
+        );
+    }
+
+    /**
+     * Error handler for unknown property mutator in Annotation class.
+     *
+     * @param string $name Unkown property name
+     * @param mixed $value Property value
+     */
+    public function __set($name, $value)
+    {
+        throw new \BadMethodCallException(
+            sprintf("Unknown property '%s' on annotation '%s'.", $name, get_class($this))
+        );
     }
 }
