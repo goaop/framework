a5efe872cb9afeede2464b1423610ac7d52e3350
Lissachenko Alexander <lisachenko.it@gmail.com>
2012-08-30 17:21:31 +0300

    Refactor DemoAspectKernel to use registration of aspect in the container

--- demos/DemoAspectKernel.php
+++ demos/DemoAspectKernel.php
@@ -6,19 +6,10 @@
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 
+use Aspect\DebugAspect;
+
 use Go\Core\AspectKernel;
 use Go\Core\AspectContainer;
-
-use Go\Aop\Support\DefaultPointcutAdvisor;
-use Go\Aop\Support\NameMatchMethodPointcut;
-use Go\Aop\Support\NameMatchPropertyPointcut;
-use Go\Aop\Framework\FieldAroundInterceptor;
-use Go\Aop\Framework\FieldBeforeInterceptor;
-use Go\Aop\Framework\ClassFieldAccess;
-use Go\Aop\Framework\MethodAfterInterceptor;
-use Go\Aop\Framework\MethodBeforeInterceptor;
-use Go\Aop\Intercept\FieldAccess;
-use Go\Aop\Intercept\MethodInvocation;
 
 /**
  * Demo Aspect Kernel class
@@ -45,47 +36,6 @@ class DemoAspectKernel extends AspectKernel
      */
     protected function configureAop(AspectContainer $container)
     {
-        $pointcut = new NameMatchMethodPointcut();
-        $pointcut->setMappedName('*');
-
-        $aspect = new Aspect\DebugAspect('ASPECT!');
-
-        // Register before advisor
-        $before        = new MethodBeforeInterceptor($this->getCallback($aspect, 'beforeMethodExecution'));
-        $beforeAdvisor = new DefaultPointcutAdvisor($pointcut, $before);
-        $container->registerAdvisor($beforeAdvisor);
-
-        // Register after advisor
-        $after        = new MethodAfterInterceptor($this->getCallback($aspect, 'afterMethodExecution'));
-        $afterAdvisor = new DefaultPointcutAdvisor($pointcut, $after);
-        $container->registerAdvisor($afterAdvisor);
-
-        // Register around field advisor
-        $fieldPointcut = new NameMatchPropertyPointcut();
-        $fieldPointcut->setMappedName('*');
-        $fieldAdvice  = new FieldAroundInterceptor($this->getCallback($aspect, 'aroundFieldAccess'));
-        $fieldAdvisor = new DefaultPointcutAdvisor($fieldPointcut, $fieldAdvice);
-        $container->registerAdvisor($fieldAdvisor);
-    }
-
-
-    /**
-     * Temporary function to return closure from aspect
-     *
-     * @param object $aspect Aspect instance
-     * @param string $methodName Method name for callback
-     *
-     * @return closure
-     */
-    private function getCallback($aspect, $methodName)
-    {
-        if (version_compare(PHP_VERSION, '5.4.0') >= 0) {
-            $refClass = new ReflectionClass($aspect);
-            return $refClass->getMethod($methodName)->getClosure($aspect);
-        } else {
-            return function () use ($aspect, $methodName) {
-                return call_user_func_array(array($aspect, $methodName), func_get_args());
-            };
-        }
+        $container->registerAspect(new DebugAspect('ASPECT!'));
     }
 }
