82dadc8e4e173dda0ba58e659205c759ff2aab70
Lissachenko Alexander <lisachenko.it@gmail.com>
2012-11-28 13:40:41 +0200

    Add example of kernel configuration to the demo

--- demos/index.php
+++ demos/index.php
@@ -10,7 +10,20 @@ include '../src/Go/Core/AspectKernel.php';
 include 'DemoAspectKernel.php';
 
 // Initialize demo aspect container
-DemoAspectKernel::getInstance()->init();
+DemoAspectKernel::getInstance()->init(array(
+    // Configuration for autoload namespaces
+    'autoload' => array(
+        'Go'               => realpath(__DIR__ . '/../src'),
+        'TokenReflection'  => realpath(__DIR__ . '/../vendor/andrewsville/php-token-reflection/'),
+        'Doctrine\\Common' => realpath(__DIR__ . '/../vendor/doctrine/common/lib/')
+    ),
+    // Default application directory
+    'appDir' => __DIR__ . '/../demos',
+    // Cache directory for Go! generated classes
+    'cacheDir' => null, //__DIR__ . '/../cache',
+    // Include paths for aspect weaving
+    'includePaths' => array(),
+));
 
 $class = new Example('test');
 $class->publicHello();
