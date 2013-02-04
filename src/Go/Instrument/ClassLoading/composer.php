b9ebf4aab86396a343e087b74d90ccd7e8ef1bb7
Lissachenko Alexander <lisachenko.it@gmail.com>
2013-02-04 16:22:03 +0200

    Use a trick with autoloading file for composer to solve a problem with prepending an autoloader #30

---
+++ src/Go/Instrument/ClassLoading/composer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2013, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace Go\Instrument\ClassLoading;
+
+use Go\Instrument\ClassLoading\UniversalClassLoader;
+
+/**
+* Ensures that autoloader of Go! library is first in the stack of autoloaders
+*
+* This fix problems with autoloaders that was prepended, for example, composer.
+*
+* @return void
+*/
+function ensureLibraryAutoloaderIsFirst()
+{
+    $loaders = spl_autoload_functions();
+    if (isset($loaders[0]) && ($loaders[0][0] instanceof UniversalClassLoader)) {
+        return;
+    }
+    $newLoaders = array();
+    foreach ($loaders as $loader) {
+        spl_autoload_unregister($loader);
+        if (is_array($loader) && ($loader[0] instanceof UniversalClassLoader)) {
+            array_unshift($newLoaders, $loader);
+        } else {
+            array_push($newLoaders, $loader);
+        }
+    }
+    foreach ($newLoaders as $loader) {
+        spl_autoload_register($loader);
+    }
+}
+
+ensureLibraryAutoloaderIsFirst();
