afce4abc43bc7d3a9a7121918b929172c0a5e53d
Alexander Lisachenko <lisachenko.it@gmail.com>
2013-06-29 16:55:50 +0400

    Better handling for the cache directory existence, resolves #62

--- src/Go/Instrument/Transformer/CachingTransformer.php
+++ src/Go/Instrument/Transformer/CachingTransformer.php
@@ -48,9 +48,24 @@ class CachingTransformer extends BaseSourceTransformer
     public function __construct(AspectKernel $kernel, array $transformers)
     {
         parent::__construct($kernel);
-        if ($this->options['cacheDir']) {
-            $this->cachePath = realpath($this->options['cacheDir']);
+        $cacheDir = $this->options['cacheDir'];
+
+        if ($cacheDir) {
+            if (!is_dir($cacheDir)) {
+                $cacheRootDir = dirname($cacheDir);
+                if (!is_writable($cacheRootDir) || !is_dir($cacheRootDir)) {
+                    throw new \InvalidArgumentException(
+                        "Can not create a directory {$cacheDir} for the cache.
+                        Parent directory {$cacheRootDir} is not writable or not exist.");
+                }
+                mkdir($cacheDir, 0770);
+            }
+            if (!is_writable($cacheDir)) {
+                throw new \InvalidArgumentException("Cache directory {$cacheDir} is not writable");
+            }
+            $this->cachePath = realpath($cacheDir);
         }
+
         $this->rootPath     = realpath($this->options['appDir']);
         $this->transformers = $transformers;
     }
@@ -77,7 +92,10 @@ class CachingTransformer extends BaseSourceTransformer
 
         // TODO: add more accurate cache invalidation, like in Symfony2
         if ($cacheModified < $lastModified || !$this->container->isFresh($cacheModified)) {
-            @mkdir(dirname($cacheUri), 0770, true);
+            $parentCacheDir = dirname($cacheUri);
+            if (!is_dir($parentCacheDir)) {
+                mkdir($parentCacheDir, 0770, true);
+            }
             $this->processTransformers($metadata);
             file_put_contents($cacheUri, $metadata->source);
             return;
