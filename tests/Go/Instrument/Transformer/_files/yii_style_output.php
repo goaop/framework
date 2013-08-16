1c36c5e0d6247d38adedc4e47dbaf87a69792cd4
Andrey 'virus' Janzen <andrey@janzen.su>
2013-08-16 13:36:26 +0700

    [fix #88] fixed includes inside function arguments

--- tests/Go/Instrument/Transformer/_files/yii_style_output.php
+++ tests/Go/Instrument/Transformer/_files/yii_style_output.php
@@ -4,3 +4,5 @@ include \Go\Instrument\Transformer\FilterInjectorTransformer::rewrite(($class->m
 include_once \Go\Instrument\Transformer\FilterInjectorTransformer::rewrite(($class->method('filename')), __DIR__);
 require \Go\Instrument\Transformer\FilterInjectorTransformer::rewrite(($class->method('filename')), __DIR__);
 require_once \Go\Instrument\Transformer\FilterInjectorTransformer::rewrite(($class->method('filename')), __DIR__);
+
+$app->bindInstallPaths(require \Go\Instrument\Transformer\FilterInjectorTransformer::rewrite( __DIR__.'/paths.php', __DIR__));
