0b0c7c500c31e74d22c6b477d89dac64a7b7f829
Alexander Lisachenko <lisachenko.it@gmail.com>
2013-08-25 21:01:50 +0400

    Add tests for field interceptors and function interceptor

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,39 +9,9 @@ ini_set('display_errors', 1);
 
 if (defined("AUTOLOAD_PATH")) {
     if (is_file(realpath(AUTOLOAD_PATH))) {
-        include_once AUTOLOAD_PATH;
+        $loader = include_once AUTOLOAD_PATH;
+        $loader->add('Go', __DIR__);
     } else {
         throw new InvalidArgumentException("Cannot load custom autoload file located at ".AUTOLOAD_PATH);
     }
-} else {
-
-    // default autoload for library classes
-    spl_autoload_register(function($class) {
-        if (0 === strpos($class, 'Go\\')) {
-            $path = __DIR__ . '/../src/' . implode('/', (explode('\\', $class))).'.php';
-            if (!file_exists($path)) {
-                return false;
-            }
-
-            require $path;
-
-            if (!class_exists($class) && !interface_exists($class)) {
-                return false;
-            }
-            return true;
-        } else if (0 === strpos($class, 'TokenReflection\\')) {
-            $path = __DIR__ . '/../vendor/andrewsville/php-token-reflection/' . implode('/', (explode('\\', $class))).'.php';
-            if (!file_exists($path)) {
-                return false;
-            }
-
-            require $path;
-
-            if (!class_exists($class) && !interface_exists($class)) {
-                return false;
-            }
-            return true;
-        }
-        return false;
-    });
 }
