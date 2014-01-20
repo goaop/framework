1aac2484f27c54105b61d89c5e8ea4b04d277712
Alexander <lisachenko.it@gmail.com>
2014-01-20 15:06:03 +0400

    Introduce NotPointFilter class

---
+++ src/Go/Aop/Support/NotPointFilter.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Go! OOP&AOP PHP framework
+ *
+ * @copyright     Copyright 2014, Lissachenko Alexander <lisachenko.it@gmail.com>
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+namespace Go\Aop\Support;
+
+use Go\Aop\PointFilter;
+
+/**
+ * Logical not filter.
+ */
+class NotPointFilter implements PointFilter
+{
+    /**
+     * Kind of filter
+     *
+     * @var int
+     */
+    private $kind = 0;
+
+    /**
+     * First part of filter
+     *
+     * @var PointFilter|null
+     */
+    private $first = null;
+
+    /**
+     * Not constructor
+     *
+     * @param PointFilter $first First part of filter
+     */
+    public function __construct(PointFilter $first)
+    {
+        $this->kind   = $first->getKind();
+        $this->first  = $first;
+    }
+
+    /**
+     * Performs matching of point of code
+     *
+     * @param mixed $point Specific part of code, can be any Reflection class
+     *
+     * @return bool
+     */
+    public function matches($point)
+    {
+        return !$this->first->matches($point);
+    }
+
+    /**
+     * Returns the kind of point filter
+     *
+     * @return integer
+     */
+    public function getKind()
+    {
+        return $this->kind;
+    }
+}
