e7bd5fffeba93dac85907a89151a0ff5ddb26093
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-04-19 23:46:07 +0400

    Enable downloading of nightly build of HHVM for travis, resolves #146

---
+++ .travis.before.sh
@@ -0,0 +1,17 @@
+set -x
+if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ]; then
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get purge -q -y hhvm
+    sudo add-apt-repository -y ppa:mapnik/boost
+    sudo apt-get update -q -y
+    sudo apt-get install -q -y hhvm-nightly
+    hhvm --version
+
+    curl -sS https://getcomposer.org/installer > composer-installer.php
+    hhvm composer-installer.php
+    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar self-update
+    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar install --dev --prefer-source
+else
+    composer self-update
+    composer install --dev --prefer-source
+fi
