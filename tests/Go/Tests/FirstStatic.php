e0dcef1f80a9f9faad70eb8572f99724061d3576
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-04-25 15:16:17 +0400

    Fix an error with staticExpects and phpunit>3.9, resolves #147

---
+++ tests/Go/Tests/FirstStatic.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Go\Tests;
+
+use Go\Aop\Framework\ClosureStaticMethodInvocation;
+
+class FirstStatic extends First
+{
+    /**
+     * @var ClosureStaticMethodInvocation|null
+     */
+    protected static $invocation = null;
+
+    public function __construct(ClosureStaticMethodInvocation $invocation)
+    {
+        static::$invocation = $invocation;
+    }
+
+    // Recursion test
+    public static function staticLsbRecursion($value, $level = 0)
+    {
+        return static::$invocation->__invoke(__CLASS__, array($value, $level));
+    }
+}
