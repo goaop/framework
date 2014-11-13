c8fe8af83e9cb40f365c6946bd0a69cfbb4e205d
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-11-13 13:26:32 +0300

    Refactor annotation pointcut classes to use only single class

---
+++ src/Aop/Pointcut/AnnotationPointcut.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Go! AOP framework
+ *
+ * @copyright Copyright 2012, Lisachenko Alexander <lisachenko.it@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Go\Aop\Pointcut;
+
+use Go\Aop\Pointcut;
+use Go\Aop\PointFilter;
+use Go\Aop\Support\AnnotationFilter;
+use Go\Aop\Support\TruePointFilter;
+
+/**
+ * Annotation property pointcut checks property annotation
+ */
+class AnnotationPointcut extends AnnotationFilter implements Pointcut
+{
+
+    /**
+     * Filter for class
+     *
+     * @var null|PointFilter
+     */
+    private $classFilter = null;
+
+    /**
+     * Set the ClassFilter to use for this pointcut.
+     *
+     * @param PointFilter $classFilter
+     */
+    public function setClassFilter(PointFilter $classFilter)
+    {
+        $this->classFilter = $classFilter;
+    }
+
+    /**
+     * Return the ClassFilter for this pointcut.
+     *
+     * @return PointFilter
+     */
+    public function getClassFilter()
+    {
+        if (!$this->classFilter) {
+            $this->classFilter = TruePointFilter::getInstance();
+        }
+
+        return $this->classFilter;
+    }
+}
