f1adcda6da9f94131b0f0ca58d805ac173530a2f
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-11-20 14:30:35 +0300

    Add new INTERCEPT_INITIALIZATIONS feature and enable registration of transformer

--- src/Aop/Features.php
+++ src/Aop/Features.php
@@ -22,29 +22,35 @@ interface Features
     const INTERCEPT_FUNCTIONS = 1;
 
     /**
+     * Enables interception of "new" operator in the source code
+     * By default this feature is disabled, because it's very tricky
+     */
+    const INTERCEPT_INITIALIZATIONS = 2;
+
+    /**
      * Enables usage of traits and introductions as well, available since PHP5.4
      */
-    const USE_TRAIT = 2;
+    const USE_TRAIT = 4;
 
     /**
      * Allows to use closures with binding, available since PHP5.4
      */
-    const USE_CLOSURE = 4;
+    const USE_CLOSURE = 8;
 
     /**
      * Enables usage of splat '...' operator, available since PHP5.6
      */
-    const USE_SPLAT_OPERATOR = 8;
+    const USE_SPLAT_OPERATOR = 16;
 
     /**
      * Do not check the cache presence and assume that cache is already prepared
      *
      * This flag is usable for read-only file systems (GAE, phar, etc)
      */
-    const PREBUILT_CACHE = 16;
+    const PREBUILT_CACHE = 32;
 
     /**
      * Allows to use 'static::class' in the source code of proxies, available since PHP5.5
      */
-    const USE_STATIC_FOR_LSB = 32;
+    const USE_STATIC_FOR_LSB = 64;
 }
