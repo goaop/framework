bcfe468b4cb3c388357f6e3ad63c4e82d026edd2
Alexander Lisachenko <lisachenko.it@gmail.com>
2015-02-07 21:05:07 +0300

    Remove unused 'id' parameter

--- src/Core/Container.php
+++ src/Core/Container.php
@@ -58,7 +58,7 @@ class Container
         if (!is_callable($value)) {
             throw new \InvalidArgumentException("Only callable values can be shared in the container");
         }
-        $value = function ($container) use ($value, $id) {
+        $value = function ($container) use ($value) {
             static $sharedValue;
 
             if (null === $sharedValue) {
