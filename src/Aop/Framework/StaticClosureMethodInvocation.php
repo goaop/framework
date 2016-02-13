4120cdbae59f9bdd5c92379e3114d28ecb27506b
Lisachenko Alexander <lisachenko.it@gmail.com>
2016-02-13 19:41:53 +0300

    Remove almost all traits and use simple inheritance with clean code

--- src/Aop/Framework/StaticClosureMethodInvocation.php
+++ src/Aop/Framework/StaticClosureMethodInvocation.php
@@ -10,11 +10,49 @@
 
 namespace Go\Aop\Framework;
 
-use Go\Aop\Framework\Block\ClosureStaticProceedTrait;
-use Go\Aop\Framework\Block\SimpleInvocationTrait;
-
-class StaticClosureMethodInvocation extends AbstractMethodInvocation
+class StaticClosureMethodInvocation extends DynamicClosureMethodInvocation
 {
-    use ClosureStaticProceedTrait;
-    use SimpleInvocationTrait;
+    /**
+     * Invokes original method and return result from it
+     *
+     * @return mixed
+     */
+    public function proceed()
+    {
+        if (isset($this->advices[$this->current])) {
+            /** @var $currentInterceptor \Go\Aop\Intercept\Interceptor */
+            $currentInterceptor = $this->advices[$this->current++];
+
+            return $currentInterceptor->invoke($this);
+        }
+
+        // Rebind the closure if scope (class name) was changed since last time
+        if ($this->previousInstance !== $this->instance) {
+            if ($this->closureToCall === null) {
+                $this->closureToCall = $this->getStaticInvoker($this->className, $this->reflectionMethod->name);
+            }
+            $this->closureToCall = $this->closureToCall->bindTo(null, $this->instance);
+            $this->previousInstance = $this->instance;
+        }
+
+        $closureToCall = $this->closureToCall;
+
+        return $closureToCall($this->arguments);
+
+    }
+
+    /**
+     * Returns static method invoker
+     *
+     * @param string $className Class name to forward request
+     * @param string $method Method name to call
+     *
+     * @return \Closure
+     */
+    protected static function getStaticInvoker($className, $method)
+    {
+        return function(array $args) use ($className, $method) {
+            return forward_static_call_array(array($className, $method), $args);
+        };
+    }
 }
