2a2baaa882eea3ed1db80adf8d5166b264fdfd70
NateC <natechicago@users.noreply.github.com>
2016-05-04 16:38:53 -0500

    Property names documented with @property should be prefixed with a $ sign

--- src/Instrument/Transformer/StreamMetaData.php
+++ src/Instrument/Transformer/StreamMetaData.php
@@ -17,18 +17,18 @@ use InvalidArgumentException;
 /**
  * Stream metadata object
  *
- * @property bool timed_out TRUE if the stream timed out while waiting for data on the last call to fread() or fgets().
- * @property bool blocked TRUE if the stream is in blocking IO mode
- * @property bool eof TRUE if the stream has reached end-of-file.
- * @property int unread_bytes the number of bytes currently contained in the PHP's own internal buffer.
- * @property string stream_type a label describing the underlying implementation of the stream.
- * @property string wrapper_type a label describing the protocol wrapper implementation layered over the stream.
- * @property mixed wrapper_data wrapper specific data attached to this stream.
- * @property array filters array containing the names of any filters that have been stacked onto this stream.
- * @property string mode the type of access required for this stream
- * @property bool seekable whether the current stream can be seeked.
- * @property string uri the URI/filename associated with this stream.
- * @property string source of the stream.
+ * @property bool   $timed_out     TRUE if the stream timed out while waiting for data on the last call to fread() or fgets().
+ * @property bool   $blocked       TRUE if the stream is in blocking IO mode
+ * @property bool   $eof           TRUE if the stream has reached end-of-file.
+ * @property int    $unread_bytes  The number of bytes currently contained in the PHP's own internal buffer.
+ * @property string $stream_type   A label describing the underlying implementation of the stream.
+ * @property string $wrapper_type  A label describing the protocol wrapper implementation layered over the stream.
+ * @property mixed  $wrapper_data  Wrapper-specific data attached to this stream.
+ * @property array  $filters       Array containing the names of any filters that have been stacked onto this stream.
+ * @property string $mode          The type of access required for this stream
+ * @property bool   $seekable      Whether the current stream can be seeked.
+ * @property string $uri           The URI/filename associated with this stream.
+ * @property string $source        The contents of the stream.
  */
 class StreamMetaData extends ArrayObject
 {
