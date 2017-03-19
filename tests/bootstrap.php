69507ef1b6a958def5190cedb3f1cda01df059c8
Lisachenko Alexander <lisachenko.it@gmail.com>
2017-03-19 11:50:40 +0300

    ini_set() accepts second argument as string

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,6 @@ declare(strict_types = 1);
  * This source file is subject to the license that is bundled
  * with this source code in the file LICENSE.
  */
-ini_set('display_errors', 1);
+ini_set('display_errors', 'on');
 
 include __DIR__ . '/../vendor/autoload.php';
