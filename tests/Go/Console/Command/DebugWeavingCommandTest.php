444ac931ef25f69b33850423c5d071dda7baa145
Nikola Svitlica a.k.a. TheCelavi <thecelavi@mac.com>
2017-08-17 15:14:00 +0200

    Fixing test to be more stabile when matching console output.

--- tests/Go/Console/Command/DebugWeavingCommandTest.php
+++ tests/Go/Console/Command/DebugWeavingCommandTest.php
@@ -10,8 +10,8 @@ class DebugWeavingCommandTest extends BaseFunctionalTest
     {
         $output = str_replace("\n", ' ', $this->execute('debug:weaving', null, false));
 
-        $this->assertRegexp('/.+aspect\/_proxies\/src\/Application\/InconsistentlyWeavedClass.php.+generated on second "warmup" pass.+/', $output);
-        $this->assertRegexp('/.+aspect\/_proxies\/src\/Application\/Main.php".+is consistently weaved.+/', $output);
+        $this->assertRegexp('/.+InconsistentlyWeavedClass.php.+generated.+on.+second.+"warmup".+pass.+/', $output);
+        $this->assertRegexp('/.+Main.php".+is.+consistently.+weaved.+/', $output);
         $this->assertContains('[ERROR] Weaving is unstable, there are 1 reported error(s).', $output);
     }
 
